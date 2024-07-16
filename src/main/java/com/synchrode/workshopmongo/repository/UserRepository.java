9b0d78619e1c454191ca7ba60c8100b9fc897afc
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-07-16 13:52:05 -0300

    using the DTO pattern to return users

--- src/main/java/com/synchrode/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/synchrode/workshopmongo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.synchrode.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{ // primeiro param - tipo da classe de domínio que ele vai gerenciar / segundo param - tipo do id da classe que está sendo gerenciada 
+public interface UserRepository extends MongoRepository<User, String>{
 
 }
