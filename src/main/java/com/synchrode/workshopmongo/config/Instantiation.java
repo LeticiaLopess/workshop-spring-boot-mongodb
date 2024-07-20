fe196d06f3de56c0bf973e0645dbf743be0340f6
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-07-20 20:44:01 -0300

    projection of author data with DTO

--- src/main/java/com/synchrode/workshopmongo/config/Instantiation.java
+++ src/main/java/com/synchrode/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.synchrode.workshopmongo.domain.Post;
 import com.synchrode.workshopmongo.domain.User;
+import com.synchrode.workshopmongo.dto.AuthorDTO;
 import com.synchrode.workshopmongo.repository.PostRepository;
 import com.synchrode.workshopmongo.repository.UserRepository;
 
@@ -44,12 +45,12 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		// no author teremos uma cópia do objeto do User, é uma cópia e não uma referência.
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
