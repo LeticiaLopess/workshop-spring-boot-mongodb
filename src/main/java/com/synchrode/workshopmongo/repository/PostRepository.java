c5a5462eff8400de4fdc71be363e99b37c5605b2
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-07-21 00:09:56 -0300

    simple search using @Query

--- src/main/java/com/synchrode/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/synchrode/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.synchrode.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.synchrode.workshopmongo.domain.Post;
@@ -11,4 +12,13 @@ import com.synchrode.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // { <field>: { $regex: /pattern/, $options: '<options>' } } 
+	List<Post> searchTitle(String text);
+	
+	/*
+	  Colocamos ?0 pois entra ali o primeiro parâmetro (text),
+	  que é o que o usuário vai digitar.Se fosse o segundo a 
+	  entrar ali, colocariamos ?1. 
+	*/
 }
